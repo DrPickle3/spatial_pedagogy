022831fc71ba766b12438c454d94bd9ddad0bcca
DrPickle3 <ronc1402@usherbrooke.ca>
2025-09-02 13:52:09 -0400

    Hello World on the display

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
-#include <Arduino.h>
-#include <iostream>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define I2C_SDA 4
+#define I2C_SCL 5
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Wire.begin(I2C_SDA, I2C_SCL);
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        // Display not found
+        for (;;);
+    }
+
+    display.clearDisplay();
+    display.setTextSize(1);              // Text size
+    display.setTextColor(SSD1306_WHITE); // White text
+    display.setCursor(0, 0);             // Top-left corner
+    display.println("Hello World !");
+    display.display();                    // Render it
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  std::cout << "Hello World !" << std::endl;
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Nothing else needed
 }
