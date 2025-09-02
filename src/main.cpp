cc82a21e46a3a64e7534ce26ab4a1f9d337edbf9
DrPickle3 <ronc1402@usherbrooke.ca>
2025-09-02 11:15:18 -0400

    project init

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <iostream>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  std::cout << "Hello World !" << std::endl;
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
