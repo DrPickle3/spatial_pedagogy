04b24f03d9670ce9b71a800a17f13425e963e08b
DrPickle3 <ronc1402@usherbrooke.ca>
2025-10-06 13:15:22 -0400

    Refactored but new panick message ...

--- scripts/indoorPositionDisplay.py
+++ scripts/indoorPositionDisplay.py
@@ -4,6 +4,7 @@ import socket
 import json
 import csv
 import re
+import utils
 import matplotlib.pyplot as plt
 
 TCP_IP = "0.0.0.0"
@@ -28,80 +29,11 @@ data, addr = sock.accept()
 print(f"***Connection accepted from {addr}***")
 
 distance_a1_a2 = 2.2098
-meter2pixel = 150
 range_offset = 0.0
 
 with open(filename, "w", newline = "") as file: #clear the file
     writer = csv.writer(file)
     # writer.writerow(["x", "y"])
-
-def turtle_init(t=turtle):
-    t.hideturtle()
-    t.speed(0)
-
-def fill_cycle(x, y, r, color="black", t=turtle):
-    t.up()
-    t.goto(x, y)
-    t.down()
-    t.dot(r, color)
-    t.up()
-
-
-def write_txt(x, y, txt, color="black", t=turtle, f=('Arial', 12, 'normal')):
-
-    t.pencolor(color)
-    t.up()
-    t.goto(x, y)
-    t.down()
-    t.write(txt, move=False, align='left', font=f)
-    t.up()
-
-
-def draw_rect(x, y, w, h, color="black", t=turtle):
-    t.pencolor(color)
-
-    t.up()
-    t.goto(x, y)
-    t.down()
-    t.goto(x + w, y)
-    t.goto(x + w, y + h)
-    t.goto(x, y + h)
-    t.goto(x, y)
-    t.up()
-
-
-def fill_rect(x, y, w, h, color=("black", "black"), t=turtle):
-    t.begin_fill()
-    draw_rect(x, y, w, h, color, t)
-    t.end_fill()
-    pass
-
-
-def clean(t=turtle):
-    t.clear()
-
-
-def draw_ui(t):
-    write_txt(-300, 250, "UWB Positon", "black",  t, f=('Arial', 32, 'normal'))
-    fill_rect(-400, 200, 800, 40, "black", t)
-    write_txt(-50, 205, "WALL", "yellow",  t, f=('Arial', 24, 'normal'))
-
-
-def draw_uwb_anchor(x, y, txt, t):
-    r = 20
-    fill_cycle(x, y, r, "green", t)
-    write_txt(x + r, y, txt,
-              "black",  t, f=('Arial', 16, 'normal'))
-
-
-def draw_uwb_tag(x, y, txt, t):
-    pos_x = -250 + int(x * meter2pixel)
-    pos_y = 150 - int(y * meter2pixel)
-    r = 20
-    fill_cycle(pos_x, pos_y, r, "blue", t)
-    write_txt(pos_x, pos_y, txt + ": (" + str(x) + "," + str(y) + ")",
-              "black",  t, f=('Arial', 16, 'normal'))
-
 
 buffer = ""
 
@@ -178,15 +110,15 @@ def main():
     t_a1 = turtle.Turtle()
     t_a2 = turtle.Turtle()
     t_a3 = turtle.Turtle()
-    turtle_init(t_ui)
-    turtle_init(t_a1)
-    turtle_init(t_a2)
-    turtle_init(t_a3)
+    utils.turtle_init(t_ui)
+    utils.turtle_init(t_a1)
+    utils.turtle_init(t_a2)
+    utils.turtle_init(t_a3)
 
     a1_range = 0.0
     a2_range = 0.0
 
-    draw_ui(t_ui)
+    utils.draw_ui(t_ui)
 
     while True:
         node_count = 0
@@ -194,15 +126,15 @@ def main():
 
         for one in list:
             if one["A"] == ANCHOR1:
-                clean(t_a1)
+                utils.clean(t_a1)
                 a1_range = float(one["R"])
-                draw_uwb_anchor(-350, 150, "ANCHOR1(0,0)", t_a1)
+                utils.draw_uwb_anchor(-350, 150, "ANCHOR1(0,0)", t_a1)
                 node_count += 1
 
             if one["A"] == ANCHOR2:
-                clean(t_a2)
+                utils.clean(t_a2)
                 a2_range = float(one["R"])
-                draw_uwb_anchor(-350 + meter2pixel * distance_a1_a2,
+                utils.draw_uwb_anchor(-350 + utils.meter2pixel * distance_a1_a2,
                                 150, "ANCHOR2(" + str(distance_a1_a2)+",0)", t_a2)
                 node_count += 1
 
@@ -218,8 +150,8 @@ def main():
                 positions.append((x, y))
             update_scatter()
 
-            clean(t_a3)
-            draw_uwb_tag(x, y, "TAG", t_a3)
+            utils.clean(t_a3)
+            utils.draw_uwb_tag(x, y, "TAG", t_a3)
 
         time.sleep(0.1)
 
