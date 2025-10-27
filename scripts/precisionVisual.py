6b754eb14cfc57eaefa6a1ef447e6f9293e5c10f
DrPickle3 <ronc1402@usherbrooke.ca>
2025-10-27 14:48:14 -0400

    arg parser

--- scripts/precisionVisual.py
+++ scripts/precisionVisual.py
@@ -1,15 +1,28 @@
+import argparse
 import utils
+
+def build_arg_parser():
+    """Build argument parser."""
+    p = argparse.ArgumentParser(
+        description="Writing position of Tag with Anchors in CSV"
+    )    
+    p.add_argument('--display', action='store_true',
+                    help='Display real time graphic of position and Anchors')
+    return p
 
 
 def main():
 
+    parser = build_arg_parser()
+    args = parser.parse_args()
+
     sock = utils.connect_wifi()
-    # utils.screen_init()
+    args.display and utils.screen_init()
     utils.clear_file()
 
     try:
         while True:
-            utils.main_loop(sock, "../images/test.png")
+            utils.main_loop(sock, "../images/test.png", args.display)
     except KeyboardInterrupt:
         pass
 
